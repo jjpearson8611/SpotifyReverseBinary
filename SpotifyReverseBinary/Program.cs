c5166c53866431e49f64e6b1448174be51e64bf8
Jason Pearson <jjpearson8611@gmail.com>
2013-06-08 08:53:29 -0400

    COMMENTS!!!
    I noticed that I didn't comment this code at all and that is bad mmmkay

--- SpotifyReverseBinary/Program.cs
+++ SpotifyReverseBinary/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//
+// This program takes a number and converts it to binary then converts the backwards binary back to decimal
+//
+//
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +14,20 @@ namespace SpotifyReverseBinary
     {
         static void Main(string[] args)
         {
+            //holds the typed in number
             int OrigNumber;
 
+            //just a simple loop to keep testing until you dont enter a whole number
             bool KeepGoing = true;
+
+            //KEEP GOING!
             while (KeepGoing)
             {
-                Console.WriteLine("Insert a decimal number");
+                //get a whole numbered int from the user
+                Console.WriteLine("Insert a whole number");
                 if (int.TryParse(Console.ReadLine(), out OrigNumber) == true)
                 {
+                    //write out the new decimal number
                     Console.WriteLine(ConvertToDecimal((ConvertToBinary(OrigNumber))));
                 }
                 else
@@ -30,25 +40,41 @@ namespace SpotifyReverseBinary
 
         public static int ConvertToDecimal(string Number)
         {
+            //counts the total
             int Counter = 0;
+
+            //counts the exponent for 2
             int Exponent = 0;
+
+            //while there are still numbers left
             while (Number.Length != 1)
             {
+                //increase counter by the right most number times 2 ^ exponent
                 Counter += (int.Parse(Number.Substring(Number.Length - 1, 1)) * (int)(Math.Pow(2, (double)Exponent)));
+                
+                //Shift right logical if you will
                 Number = Number.Substring(0, Number.Length - 1);
+                
+                //increase the exponent number
                 Exponent++;
             }
 
+            //increase the counter for the last number
             Counter += int.Parse(Number) * (int)(Math.Pow(2, (double)Exponent));
 
+            //return the final decimal number
             return Counter;
         }
 
         public static string ConvertToBinary(int Number)
         {
+            //string to hold the returned binary number
             string Returning = string.Empty;
+
+            //keep going until there is only one 0 or 1 left in the number
             while (Number != 0 && Number != 1)
             {
+                //determine if the binary number is 0 or 1
                 if ((Number % 2) == 1)
                 {
                     Returning += "1";
@@ -57,11 +83,15 @@ namespace SpotifyReverseBinary
                 {
                     Returning += "0";
                 }
+
+                //divide the number by 2 and do it again
                 Number = Number / 2;
             }
 
+            //add the last 0 or 1 to the number
             Returning = Returning + Number.ToString();
 
+            //return the binary number
             return Returning;
         }
     }
